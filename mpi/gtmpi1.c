85455f35c05bc1d3f0fc618c8b710aa3299d0f3a
Patel, Aditi N <apatel768@gatech.edu>
2023-10-09 01:34:22 -0400

    mpi, need to test it

--- mpi/gtmpi1.c
+++ mpi/gtmpi1.c
@@ -1,16 +1,42 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <stdio.h>
+#include <math.h> //added
 #include "gtmpi.h"
 
-void gtmpi_init(int num_processes){
-
+// Global variables
+static int num_procs;
+static int rank;
+static int num_rounds;
+static int *send_flags;    // buffers
+static int *receive_flags; // buffers
+void gtmpi_init(int num_processes)
+{
+    num_procs = num_processes;
+    rank = -1; // initialize to invalid value in the beginning
+    num_rounds = (int)ceil((log2(num_procs)));
+    send_flags = (int *)malloc(num_rounds * sizeof(int));
+    receive_flags = (int *)malloc(num_rounds * sizeof(int));
+    for (int i = 0; i < num_rounds; i++)
+    {
+        send_flags[i] = 1;
+        receive_flags[i] = 1;
+    }
 }
 
-void gtmpi_barrier(){
-
+void gtmpi_barrier()
+{
+    for (int round = 0; round < num_rounds; round++)
+    {
+        int send_pt = (rank + (1 << round)) % num_procs;                      // get the partner to send to
+        int recv_pt = (rank - (1 << round) + num_procs) % num_procs;          // get the node to receive the message from
+        MPI_Send(&send_flags[round], 1, MPI_INT, send_pt, 0, MPI_COMM_WORLD); // should 2nd param be 0?
+        MPI_Recv(&receive_flags[round], 1, MPI_INT, recv_pt, 0, MPI_COMM_WORLD);
+    }
 }
 
-void gtmpi_finalize(){
-
+void gtmpi_finalize()
+{
+    return;
 }
+// main method provided -  in harness.c
--- mpi/gtmpi2.c
+++ mpi/gtmpi2.c
@@ -1,16 +1,97 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <stdio.h>
+#include <math.h> //added
 #include "gtmpi.h"
 
-void gtmpi_init(int num_processes){
+// Global struct node
+typedef struct MCSNode
+{
+    int arrChildPt[4]; // max children for arrival tree
+    int arrParentPt;
+    int wakeupParentPt;
+    int wakeupChildPt[2]; // max 2- binary tree
+} MCSNode;
 
+static MSCNode *node;
+static int rank;
+static int num_proc;
+
+void gtmpi_init(int num_processes)
+{
+    num_proc = num_processes;
+    MPI_Comm_Rank(MPI_COMM_WORLD, &rank);
+    node = (MCSNode *)malloc(sizeof(MCSNode));
+    // Initialize arrival tree
+    for (int i = 0; i < 4; i++)
+    {
+        int childRank = (4 * rank) + i + 1;
+        if (childRank < num_proc)
+        {
+            node->arrChildPt[i] = childRank;
+        }
+        else
+        {
+            node->arrChildPt[i] = -1;
+        }
+    }
+    if (rank != 0)
+    {
+        node->arrParentPt = (rank - 1) / 4;
+        node->wakeupParentPt = (rank - 1) / 2;
+    }
+    else
+    {
+        node->arrParentPt = -1;
+        node->wakeupParentPt = -1;
+    }
+    // initialize the wakeup tree
+    for (int i = 0; i < 2; i++)
+    {
+        int childRank = (2 * rank) + i + 1;
+        if (childRank < num_proc)
+        {
+            node->wakeupChildPt[i] = childRank;
+        }
+        else
+        {
+            node->wakeupChildPt[i] = -1;
+        }
+    }
 }
 
-void gtmpi_barrier(){
-
+void gtmpi_barrier()
+{
+    unsigned char sense = 1;
+    // Synchronization for arrival tree
+    for (int i = 0; i < 4; i++)
+    {
+        if (node->arrChildPt[i] != -1)
+        {
+            MPI_Recv(&sense, 1, MPI_BYTE, node->arrChildPt[i], 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        if (rank != 0)
+        {
+            MPI_Send(&sense, 1, MPI_BYTE, node->arrParentPt, 1, MPI_COMM_WORLD);
+            MPI_Recv(&sense, 1, MPI_BYTE, node->wakeupParentPt, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        // udpate sense
+        sense = -sense; // QUESTION- do we need sense variable
+        for (int i = 0; i < 2; i++)
+        {
+            if (node->wakeupChildPt[i] != -1)
+            {
+                MPI_Send(&sense, 1, MPI_BYTE, node->wakeupChildPt[i], 1, MPI_COMM_WORLD);
+            }
+        }
+    }
 }
 
-void gtmpi_finalize(){
-
+void gtmpi_finalize()
+{
+    if (node != NULL)
+    {
+        free(node);
+        node = NULL;
+    }
 }
